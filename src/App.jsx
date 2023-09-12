b528baf4de4ff91c1613a88a07177e2f666f8a1d
1Fenix7 <userpk66@gmail.com>
2023-09-13 01:58:52 +0300

    feat(api)!: add films API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navigation from './components/Navigation';
 import Main from './components/Main';
 import Results from './components/Results';
@@ -8,29 +8,6 @@ import MoviesList from './components/MoviesList';
 import Summary from './components/Summary';
 import WatchedList from './components/WatchedList';
 
-const tempMovieData = [
-	{
-		imdbID: 'tt1375666',
-		Title: 'Inception',
-		Year: '2010',
-		Poster:
-			'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-	},
-	{
-		imdbID: 'tt0133093',
-		Title: 'The Matrix',
-		Year: '1999',
-		Poster:
-			'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-	},
-	{
-		imdbID: 'tt6751668',
-		Title: 'Parasite',
-		Year: '2019',
-		Poster:
-			'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
-	},
-];
 const tempWatchedData = [
 	{
 		imdbID: 'tt1375666',
@@ -54,9 +31,32 @@ const tempWatchedData = [
 	},
 ];
 
+const query = 'Interstellar';
+const KEY = 'd3aa8c7c093e730dd5f18876de8fd3f3';
+const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${KEY}`;
+
+async function fetchMovies(setMovies) {
+	try {
+		const response = await fetch(url);
+		const result = await response.json();
+		console.log('Before filter:', result.results);
+		const filtredResults = result.results.filter(
+			(movie) => movie.poster_path || (movie.backdrop_path && movie.release_date),
+		);
+		console.log('After filter:', filtredResults);
+		setMovies(filtredResults);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
 export default function App() {
-	const [movies, setMovies] = useState(tempMovieData);
+	const [movies, setMovies] = useState([]);
 	const [watched, setWatched] = useState(tempWatchedData);
+
+	useEffect(function () {
+		fetchMovies(setMovies);
+	}, []);
 	return (
 		<>
 			<Navigation>
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,12 +1,18 @@
 function Movie({ movie }) {
+	const year = new Date(movie.release_date).getFullYear();
 	return (
 		<li>
-			<img src={movie.Poster} alt={`${movie.Title} poster`} />
-			<h3>{movie.Title}</h3>
+			<img
+				src={`https://image.tmdb.org/t/p/w500/${
+					movie.poster_path ? movie.poster_path : movie.backdrop_path
+				}`}
+				alt={`${movie.title} poster`}
+			/>
+			<h3>{movie.title}</h3>
 			<div>
 				<p>
 					<span>🗓</span>
-					<span>{movie.Year}</span>
+					<span>{`${year}`}</span>
 				</p>
 			</div>
 		</li>
